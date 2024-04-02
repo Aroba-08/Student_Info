350584522a55153fc0eebe1afb5ae5c57280540f
admin <admin@gmail.com>
2024-04-02 11:01:03 +0800

    registration

--- studentapp/views.py
+++ studentapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def login_user(request):
+    # check to see if logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('login')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('login')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
